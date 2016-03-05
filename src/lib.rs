2f8540cba2df57cdc678000bd61998f8ed04762b
Jean Piere Dudey <jeandudey@hotmail.com>
2016-03-05 09:23:01 -0430

    docs(module): Move crate documentation to top.

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,3 @@
-/**
- *    Copyright (C) 2016 Jean Piere Dudey
- *
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 //! This crate provides a native and easy way to read and write
 //! *Grand Theft Auto: Vice City* settings file (gta_vc.set).
 //!
@@ -40,6 +23,23 @@
 //! let mut settings = GtaVcSettings::new();
 //! settings.read("./tests/gta_vc.set");
 //! ```
+
+/**
+ *    Copyright (C) 2016 Jean Piere Dudey
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 extern crate byteorder;
 
